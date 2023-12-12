f49707ed6f514e9c9c998d7ce504867ed42b6dbd
Fatima <fatimasshaker@gmail.com>
2023-12-12 20:14:04 +0300

    updated the cart view and template

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Product, Cart, CartItem
-
 
 def store_view(request):
     item_objects = Product.objects.all()
@@ -8,39 +8,42 @@ def store_view(request):
     return render(request, 'store.html', context)
 
 def cart_view(request):
-   cart_items = CartItem.objects.filter(cart__user=request.user)
-   cart = request.user.cart
-   context = {'cart_items': cart_items,'cart':cart}
-   return render(request, 'cart.html', context)
+    try:
+        cart_items = CartItem.objects.filter(cart__user=request.user)
+        cart = request.user.cart
+    except ObjectDoesNotExist:
+        cart_items = []
+        cart = None
+
+    context = {'cart_items': cart_items, 'cart': cart}
+    return render(request, 'cart.html', context)
 
 def add_to_cart(request, product_id):
     product = Product.objects.get(pk=product_id)
     cart, created = Cart.objects.get_or_create(user=request.user)
     cart_item, item_created = CartItem.objects.get_or_create(cart=cart, product=product)
-       
+
     if not item_created:
         cart_item.quantity += 1
         cart_item.save()
-       
+
     return redirect('store:store')
 
-
 def increase_cart_item(request, product_id):
-   product = Product.objects.get(pk=product_id)
-   cart = request.user.cart
-   cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
-   cart_item.quantity += 1
-   cart_item.save()
-   return redirect('store:cart')
-
+    product = Product.objects.get(pk=product_id)
+    cart = request.user.cart
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('store:cart')
 
 def decrease_cart_item(request, product_id):
-   product = Product.objects.get(pk=product_id)
-   cart = request.user.cart
-   cart_item = cart.cartitem_set.get(product=product)
-   if cart_item.quantity > 1:
-       cart_item.quantity -= 1
-       cart_item.save()
-   else:
-       cart_item.delete()
-   return redirect('store:cart')
+    product = Product.objects.get(pk=product_id)
+    cart = request.user.cart
+    cart_item = cart.cartitem_set.get(product=product)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('store:cart')
